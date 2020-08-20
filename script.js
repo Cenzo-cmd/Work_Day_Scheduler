ff41981e9beb49290938f1a68df7dc4e2d80e314
Vincent <vrdphone@gmail.com>
2020-08-20 14:31:05 -0500

    updated javascript file to fix planner look

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     var now = moment();
     var currentDay = moment().format("MMM Do yy");
     var justDay = moment().format("dddd");
+    var justHour = moment().format("h:00 a");
+    console.log(justHour);
 
 
 
@@ -11,39 +13,35 @@ $(document).ready(function() {
 
     var leftCol = $(".myCol1");
 
-    function createRows() {
-        var addRow = $("<div></div");
+
+
+
+    function makeCalendar() {
+        var addRow = $("<div></div>");
         addRow.addClass("row border-dark myRow");
         $(".myContainer").append(addRow);
-    }
 
-    function createCol1() {
         var addCol1 = $("<div></div>");
         addCol1.addClass("col-2 border-dark myCol1");
-        $(".myRow").append(addCol1);
-    }
+        addRow.append(addCol1);
 
-    function createCol2() {
-        var addCol2 = $("<input></input>");
+
+        var addCol2 = $("<input>");
         addCol2.addClass("col-8 border-dark text-center myCol2");
         addCol2.attr("placeholder", "Click here to type");
-        $(".myRow").append(addCol2);
-    }
+        addRow.append(addCol2);
 
-    function createCol3() {
+
         var addCol3 = $("<div></div>");
         addCol3.addClass("col-2 border-dark myCol3");
-        $(".myRow").append(addCol3);
+        addRow.append(addCol3);
     }
+
 
 
 
     for (var i = 6; i < 20; i++) {
-
-        createRows();
-        createCol1();
-        createCol2();
-        createCol3();
+        makeCalendar();
 
 
     }
