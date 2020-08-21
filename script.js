98e242f92c4666afe2e4fc07def61726134c2ba3
Vincent <vrdphone@gmail.com>
2020-08-21 18:55:18 -0500

    added local storage

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var now = moment();
     var currentDay = moment().format("MMM Do yy");
     var justDay = moment().format("dddd");
-    var justHour = moment().format("h:00 a");
+    var justHour = moment().format("hh:00 a");
     console.log(justHour);
 
 
@@ -15,15 +15,14 @@ $(document).ready(function() {
 
     var hourBlocks = [];
 
-    for (let hour = 7; hour < 19; hour++) {
+    for (let hour = 7; hour < 21; hour++) {
         hourBlocks.push(moment({ hour }).format("hh:mma"));
         // console.log(hourBlocks);
     }
 
-    hourBlocks.forEach(function(hourBlocks) {
 
-
-        console.log("these are the hour blocks" + hourBlocks);
+    hourBlocks.forEach(function(hourBlock) {
+        let storedItem = localStorage.getItem(hourBlock);
 
         var addRow = $("<div></div>");
         addRow.addClass("row border-dark myRow");
@@ -31,14 +30,15 @@ $(document).ready(function() {
 
         var addCol1 = $("<div></div>");
         addCol1.addClass("col-2 border-dark myCol1");
-        addCol1.text(hourBlocks);
+        addCol1.text(hourBlock);
         addRow.append(addCol1);
 
 
         var addCol2 = $("<input>");
         addCol2.addClass("col-8 border-dark text-center myCol2");
         addCol2.attr("placeholder", "Click here to type");
-        addCol2.attr("id", hourBlocks);
+        addCol2.attr("id", hourBlock);
+        addCol2.val(storedItem);
         addRow.append(addCol2);
 
 
@@ -51,6 +51,25 @@ $(document).ready(function() {
         addCol3.append(addButton);
         addRow.append(addCol3);
 
+        // blockColor();
+
     })
+
+    // function blockColor(hourBlock, id) {
+    //     if (now.isBefore(hourBlock, "hour")) {
+    //         console.log(hourBlock);
+    //         console.log("hi");
+
+    //     }
+    // }
+
+
+    $(".saveButton").on("click", function() {
+        let textInput = $(this).parent().siblings("input").val();
+        let idEl = $(this).parent().siblings("input").attr("id");
+
+        localStorage.setItem(idEl, textInput);
+    })
+
 
 })
