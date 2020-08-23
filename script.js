f035a14bdc8dea1f784b6021293e19ebffd521c2
Vincent <vrdphone@gmail.com>
2020-08-22 19:21:37 -0500

    added footer, finished all JS code, added clear button

--- script.js
+++ script.js
@@ -3,23 +3,22 @@ $(document).ready(function() {
     var now = moment();
     var currentDay = moment().format("MMM Do yy");
     var justDay = moment().format("dddd");
-
-    $("#currentDay").text(justDay + " " + currentDay);
-
+    var currentTime = moment().format("LT");
+    console.log(currentTime);
     var hourBlocks = [];
 
-    for (let hour = 7; hour < 24; hour++) {
+    $("#currentDay").text(justDay + " " + currentDay);
+    $("#currentTime").text(currentTime);
+
+
+    for (let hour = 7; hour < 23; hour++) {
         hourBlocks.push(moment({ hour }).format("hh:mma"));
     }
 
     hourBlocks.forEach(function(hourBlock) {
-        console.log(hourBlock);
         elementId = hourBlock.replace(/:/, "");
-        console.log(elementId);
         let storedItem = localStorage.getItem(elementId);
         var blockTime = moment(hourBlock, 'h:mm a');
-
-
 
         var addRow = $("<div></div>");
         addRow.addClass("row border-dark myRow");
@@ -34,8 +33,8 @@ $(document).ready(function() {
         var addCol2 = $("<input>");
         addCol2.addClass("col-8 border-dark text-center myCol2");
         addCol2.attr("placeholder", "Click here to type");
+        addCol2.addClass("mainContent");
         addCol2.attr("id", elementId);
-        addCol2.addClass("hello");
         addCol2.val(storedItem);
         addRow.append(addCol2);
 
@@ -50,27 +49,34 @@ $(document).ready(function() {
         blockColor(blockTime, elementId);
     })
 
+    var clearBtn = $("<button>" + "Clear All Saved Events" + "</button>");
+    clearBtn.addClass("clearButton");
+    $("#buttons").append(clearBtn);
+
     $(".saveButton").on("click", function() {
         let textInput = $(this).parent().siblings("input").val();
         var idEl = $(this).parent().siblings("input").attr("id");
         console.log("you clicked me");
-
         localStorage.setItem(idEl, textInput);
+    })
+
+    $(".clearButton").on("click", function() {
+        localStorage.clear();
+        location.reload();
     })
 
     function blockColor(blockTime, elementId) {
         if (now.isSame(blockTime, "hour")) {
             getIdEl = "#" + elementId;
-            $(getIdEl).addClass("yellow");
+            $(getIdEl).addClass("present");
         }
         if (now.isBefore(blockTime, "hour")) {
             getIdEl = "#" + elementId;
-            $(getIdEl).addClass("red");
+            $(getIdEl).addClass("future");
         }
         if (now.isAfter(blockTime, "hour")) {
             getIdEl = "#" + elementId;
-            $(getIdEl).addClass("green");
+            $(getIdEl).addClass("past");
         }
     }
-
 })
